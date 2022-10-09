6d3603acea26f8df91d197139bc3bea75a4be051
xianchiz <112578482+xianchiz@users.noreply.github.com>
2022-10-09 11:40:37 +0000

    draft

--- bash_command/project.sh
+++ bash_command/project.sh
@@ -1,12 +1,56 @@
 #!/usr/bin/env bash
 
-echo "How many rows do you want to analyze?"
-read ROWS 
+echo "How many countries do you want to analyze randomly?"
+read LINES
+echo "Generate a new small analysis file randomly"
+shuf -n $LINES unemployment_analysis.csv > small.csv
+echo "First 3 rows for new file"
+head -n 3 small.csv
 
-COUNT = 1
-while [$COUNT -le $ROWS]
+echo "Last 3 rows for new file"
+tail -n 3 small.csv
+
+echo "Let we use a more comprehensive way: What country's unemployment data you want to search?"
+read COUNTRY 
+
+echo "Generate $COUNTRY data"
+grep $COUNTRY unemployment_analysis.csv
+
+echo "What two countries do you want to compare?"
+read COUNTRY1
+read COUNTRY2
+
+echo "Generate compared dataset"
+grep -e $COUNTRY1 -e $COUNTRY2 unemployment_analysis.csv > new_file.csv
+
+echo "The average is: "
+sum=0
+count=1
+for num in $COUNTRY1
 do
-    echo "$COUNT" >> small_unemployment_analysis.txt 
-    ((COUNT++))
-done 
+    if [ $count -le 1 ]
+    then
+        p=$num
+    else
+        sum=$(($sum + $num))
+    fi
+    ((count++))
+done
+echo "$sum/$p"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
